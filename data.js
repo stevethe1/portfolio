8432fc16e4efd935230b8c7ef3cd6a0bee26b856
stevethe1 <104700823+stevethe1@users.noreply.github.com>
2022-05-05 19:46:58 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+// src/data.js
+
+import logo from "./images/react-native-UX-design.gif"; // with import
+import htmlLogo from "./images/htmlLogo.jpg"; // with import
+import cssLogo from "./images/cssLogo.jpg"; // with import
+import JsLogo from "./images/JsLogo.gif"; // with import
+
+
+export const projects = [
+  {
+    title: "React ",
+    subtitle: "basic understand of react",
+    description:
+      "Built my portfolio with react",
+    image:logo,
+    link: "",
+  },
+  {
+    title: "HTML",
+    subtitle: "HTML",
+    description:
+      "Basic understanding of html",
+    image: htmlLogo,
+    link: "",
+  },
+  {
+    title: "CSS",
+    subtitle: " basic understanding of css",
+    description:
+      "basic CSS",
+    image: cssLogo,
+    link: "",
+  },
+  {
+    title: "JavaScript",
+    subtitle: "basic understanding of Javascript",
+    description:
+      "JavaScript",
+    image:JsLogo,
+    link: "",
+  },
+];
