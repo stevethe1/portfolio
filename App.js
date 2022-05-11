8484d0dc0edf03ae3602260bf3308cf5f1341e27
stevethe1 <104700823+stevethe1@users.noreply.github.com>
2022-05-11 09:34:48 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import './App.css';
+
+function App() {
+  return (
+  <main>
+   <Navbar />
+   <About />
+   <Projects />
+   <Contact />
+    </main>
+  );
+}
+
+export default App;
